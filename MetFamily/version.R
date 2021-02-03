481bca33524e461160f859099018dfc46a6235eb
Steffen Neumann <sneumann@ipb-halle.de>
2021-02-03 22:08:29 +0100

    Bump metFamilyBuilt to 1.3.0

--- MetFamily/version.R
+++ MetFamily/version.R
@@ -1,7 +1,7 @@
 ## MetFamily properties
 toolName       <- "MetFamily"
 toolVersion    <- "1.0"    
-metFamilyBuilt <- "1.2.0"
+metFamilyBuilt <- "1.3.0"
 
 if ("MetFamily" %in% rownames(installed.packages())) {
   packageVersion <- package.version("MetFamily")
