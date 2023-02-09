986292e0b2104194e3e147b9c18808be7a0fa00f
Steffen Neumann <sneumann@ipb-halle.de>
2023-02-09 17:35:25 +0100

    Update version string

--- MetFamily/version.R
+++ MetFamily/version.R
@@ -1,7 +1,7 @@
 ## MetFamily properties
 toolName       <- "MetFamily"
 toolVersion    <- "1.0"    
-metFamilyBuilt <- "1.3.1"
+metFamilyBuilt <- "1.3.2"
 
 if ("MetFamily" %in% rownames(installed.packages())) {
   packageVersion <- package.version("MetFamily")
