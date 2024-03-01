52bdf326187a8c26717df85459e26cbe250cb237
Steffen Neumann <sneumann@ipb-halle.de>
2024-03-01 22:40:26 +0100

    Add test for processMS1data

---
+++ tests/testthat/test_dataprocessing.R
@@ -0,0 +1,25 @@
+test_that("metaboliteProfileParsing works", {
+  load(system.file("data/testdata/processMS1data.Rdata", package = "MetFamily"))
+
+  d <- dataColumnsNameFunctionFromGroupIndex(groupIdx = 2, sampleNamesToExclude = NA)
+  
+  ## Test dimensions  
+  expect_equal(length(d), 3)
+  
+  result <- processMS1data(sampleNamesToExclude=sampleNamesToExclude,
+                           numberOfMS1features=numberOfMS1features,
+                           precursorLabels=precursorLabels,
+                           groups=c("TRI", "LVS"),
+                           metaboliteProfileColumnNames=metaboliteProfileColumnNames,
+                           dataColumnIndecesFunctionFromGroupIndex=dataColumnIndecesFunctionFromGroupIndex,
+                           dataColumnsNameFunctionFromGroupIndex=dataColumnsNameFunctionFromGroupIndex,
+                           dataColumnsNameFunctionFromGroupName=dataColumnsNameFunctionFromGroupName,
+                           dataColumnsNameFunctionFromGroupNames=dataColumnsNameFunctionFromGroupNames,
+                           groupNameFunctionFromDataColumnName=groupNameFunctionFromDataColumnName,
+                           tagsSector=tagsSector,
+                           metaboliteProfile=metaboliteProfile,
+                           progress=FALSE)
+
+expect_equal(min(result$dataFrameMeasurements[,1]), 0)
+
+})
