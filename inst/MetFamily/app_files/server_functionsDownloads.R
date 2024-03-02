48687696382c1fbb54acc939b6511e6d4af001de
Steffen Neumann <sneumann@ipb-halle.de>
2024-03-02 17:54:40 +0100

    No functional change, reindent

--- inst/MetFamily/app_files/server_functionsDownloads.R
+++ inst/MetFamily/app_files/server_functionsDownloads.R
@@ -139,17 +139,15 @@ createExportMatrix <- function(precursorSet){
 }
 
 writeTable <- function(precursorSet, file){
-  #dataFrame <- createExportMatrix(precursorSet)
-  #gz1 <- gzfile(description = file, open = "w")
-  #write.table(x = dataFrame, file = gz1, sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
-  #close(gz1)
-  show_modal_spinner(spin="scaling-squares", text="\nMerging project files to csv. This can take several minutes!")
+  show_modal_spinner(spin="scaling-squares", 
+                     text="\nMerging project files to csv. This can take several minutes!")
   lines <- createExportMatrix(precursorSet)
   gz1 <- gzfile(description = file, open = "w")
   writeLines(text = lines, con = gz1)
   close(gz1)
   remove_modal_spinner()
 }
+
 ## individual downloads
 output$downloadGlobalMS2filteredPrecursors <- downloadHandler(
   filename = function() {
@@ -201,15 +199,19 @@ output$downloadSelectedPrecursors <- downloadHandler(
   },
   contentType = 'text/csv'
 )
+
 #Obvserve button for exporting the project
 observeEvent(input$prepareAllPrecursors, {
   ExportMatrixName <<- createExportMatrixName()
   precursorSet <- 1:dataList$numberOfPrecursors
   writeTable(precursorSet = precursorSet, file = file.path(tempdir(),ExportMatrixName))
-  showModal(modalDialog(title = "Download", footer = NULL, size="s", fluidRow(column(12, p("Your download is ready."))), 
-                        fluidRow(column(3,downloadButton(outputId = "downloadAllpreparedPrecursors", label = "Download project"))),
+  showModal(modalDialog(title = "Download", footer = NULL, size="s", 
+                        fluidRow(column(12, p("Your download is ready."))), 
+                        fluidRow(column(3,downloadButton(outputId = "downloadAllpreparedPrecursors", 
+                                                         label = "Download project"))),
   ))
 })
+
 #Serving the modal with the download button to download the project
 output$downloadAllpreparedPrecursors <- downloadHandler(
   filename <- ExportMatrixName, 
@@ -220,6 +222,7 @@ output$downloadAllpreparedPrecursors <- downloadHandler(
   },
   contentType = 'text/csv'
 )
+
 ## download selected
 output$downloadHcaSelectedPrecursors <- downloadHandler(
   filename = function() {
@@ -238,6 +241,7 @@ output$downloadHcaSelectedPrecursors <- downloadHandler(
   },
   contentType = 'text/csv'
 )
+
 output$downloadImportParameterSet <- downloadHandler(
   filename = function() {
     createImportParameterSetExportFileName()
@@ -248,6 +252,7 @@ output$downloadImportParameterSet <- downloadHandler(
   },
   contentType = 'text/csv'
 )
+
 ## download images
 output$downloadHcaImage <- downloadHandler(
   filename = function() {
@@ -260,24 +265,9 @@ output$downloadHcaImage <- downloadHandler(
   }#,
   #contentType = 'image/png'
 )
+
 plotHCA <- function(file, fileType, plotMS2 = TRUE){
-  ## 1 den    ## 2 hea
-  ## 3 ms2    ## 4 l anno
-  ## 5 l sel  ## 6 l hea
-  ## 7 l ms2
-  ## 
-  ## 1 4
-  ## 1 5
-  ## 1 6
-  ## 2 7
-  ## 3 7
-  ## 
-  
   ## parameters
-  ##widthInInch     <- 10
-  ##heigthInInch    <- ifelse(test = plotMS2, yes = 7.5, no = (5.2-1.5)/5.2 * 7.5)  
-  ##resolutionInDPI <- 600
-  ### changing resolution DPI to 650
   widthInInch     <- 10
   heigthInInch    <- ifelse(test = plotMS2, yes = 7.5, no = (5.2-1.5)/5.2 * 7.5)
   resolutionInDPI <- 650
@@ -286,7 +276,10 @@ plotHCA <- function(file, fileType, plotMS2 = TRUE){
   
   switch(fileType,
          "png"={
-           png(filename = file, width = widthInPixel, height = heightInPixel, res = resolutionInDPI, bg = "white")
+           png(filename = file, 
+               width = widthInPixel, 
+               height = heightInPixel, 
+               res = resolutionInDPI, bg = "white")
          },
          "svg"={
            svg(filename = file)
@@ -297,32 +290,13 @@ plotHCA <- function(file, fileType, plotMS2 = TRUE){
          stop(paste("Unknown file type (", fileType, ")!", sep = ""))
   )
   
-  if(plotMS2){
-    
-    ########################
+  if(plotMS2) {
     graphics::layout(
       mat = matrix(
-        ### Just commenting this
-        ##data = c(1, 1, 1, 1, 2, 3,
-        ##         4, 5, 6, 7, 8, 8),
-        #data = c(1, 1, 1, 1, 2, 3),
-        #nrow = 6, ncol = 2),
-        ############################
         data = c(1, 1, 1, 1, 2, 2,3, 4, 5, 5,5, 5), nrow = 6, ncol = 2),
-      widths = c(4.58, 1.59), # this is working good
-      #####################
-      ### This is original
-      #heights = c(0.002, 0.001, 0.002, 0.0012, 0.0013, 0.0001)
-      #heights = c(0.2, 0.1, 0.2, 0.2, 0.013, 0.11) # this working good
-      #heights = c(0.0052, 0.011, 0.01112, 0.01112, 0.001113, 0.0011)
-      ##heights = c(0.0052, 0.011, 0.01112, 0.0112, 0.0113, 0.011) # this is working well
-      ###
-      heights = c(0.0052, 0.011, 0.01112, 0.01112,0.001111, 0.0111) # this is working well
-      
+      widths = c(4.58, 1.59), 
+      heights = c(0.0052, 0.011, 0.01112, 0.01112,0.001111, 0.0111)
     )
-    ################################
-    
-    
   } else {
     graphics::layout(
       mat = matrix(
@@ -334,41 +308,17 @@ plotHCA <- function(file, fileType, plotMS2 = TRUE){
     )
   }
   
-  ### i am changing the cex to 0.4 to 0.3
-  #cex <- par("cex")
-  #par(cex = 0.3)
-  ## 1
-  #drawDendrogramPlotImpl()
-  #par(cex = cex)
-  ## 2
-  #drawHeatmapPlotImpl() ## out for plotly and adapt layout
-  ## 3
-  #if(plotMS2)  drawMS2PlotImpl()
-  ## 4
-  #drawDendrogramLegendImpl()
-  ## 5
-  #drawHeatmapLegendImpl()
-  ## 6
-  #if(plotMS2)  drawMS2LegendImpl()
-  ## 7
-  #drawFragmentDiscriminativityLegendImpl()
-  ## 8
-  #drawAnnotationLegendForImageHCAimpl()
-  #drawAnnotationLegendImpl()
-  ###############################
   drawDendrogramPlotImpl() #1
-  drawHeatmapPlotImpl() # 2
+  drawHeatmapPlotImpl() #2
   
   drawFragmentDiscriminativityLegendImpl() #4
   drawHeatmapLegendImpl() #3
   
-  
   drawAnnotationLegendForImageHCAimpl() #5
-  
-  ####################
-  
+
   dev.off()
 }
+
 output$downloadPcaImage <- downloadHandler(
   filename = function() {
     fileType <- input$downloadPcaImageType
@@ -377,34 +327,23 @@ output$downloadPcaImage <- downloadHandler(
   content = function(file) {
     fileType <- input$downloadPcaImageType
     plotPCA(file, fileType)
-  }#,
-  #contentType = 'image/png'
+  }
 )
+
 plotPCA <- function(file, fileType, plotMS2 = TRUE){
-  ## 1 score  ## 2 loadings
-  ## 3 ms2    ## 4 l anno
-  ## 5 l sel  ## 6 l hea
-  ## 7 l ms2
-  ## 
-  ## 1 2 4
-  ## 1 2 5
-  ## 1 2 6
-  ## 1 2 7
-  ## 3 3 7
-  ## 
-  
   ## parameters
   widthInInch     <- 10
-  #widthInInch     <- 10 * 4 / 5
   heigthInInch    <- ifelse(test = plotMS2, yes = 6, no = (5.2-1.5)/5.2 * 6)  
-  #heigthInInch    <- 6 * 4 / 5
   resolutionInDPI <- 650
   widthInPixel    <- widthInInch  * resolutionInDPI
   heightInPixel   <- heigthInInch * resolutionInDPI
   
   switch(fileType,
          "png"={
-           png(filename = file, width = widthInPixel, height = heightInPixel, res = resolutionInDPI, bg = "white")
+           png(filename = file, 
+               width = widthInPixel, 
+               height = heightInPixel, 
+               res = resolutionInDPI, bg = "white")
          },
          "svg"={
            svg(filename = file)
@@ -416,16 +355,12 @@ plotPCA <- function(file, fileType, plotMS2 = TRUE){
   )
   
   if(plotMS2){
-    #############################
     graphics::layout(
       mat = matrix(
-        ################################
         data = c(1,1,2,2,1,1,2,2,3,3,4,4,3,3,4,4,5,5,5,5,6,6,6,6,7,7,7,7),nrow=4,ncol=7),
       widths =  c(0.85, 1.15),
       heights = c(1.02,1.01)
-      ########################
     )
-    ######################################### 
   } else {
     graphics::layout(
       mat = matrix(
@@ -436,21 +371,15 @@ plotPCA <- function(file, fileType, plotMS2 = TRUE){
       widths = c(2, 2, 1), 
       heights = c(0.7, 0.6, 2.4)
     )
-    ########## Adding this new
-    ############################
   }
   
-  ############################################
   drawPcaScoresPlotImpl1()
   calcPlotScoresGroupsLegendForImage1(scoresGroups$grouXXXps, scoresGroups$colors, 30)
   drawPcaLoadingsPlotImpl()
   drawAnnotationLegendForImagePCAimpl()
-  ##plot.new()
-  ####################
-  #################### This is the end #######
   dev.off()
 }
-##############################################
+
 output$downloadDistanceMatrix <- downloadHandler(
   filename = function() {
     createExportDistanceMatrixName(currentDistanceMatrixObj$distanceMeasure)
@@ -460,6 +389,7 @@ output$downloadDistanceMatrix <- downloadHandler(
   },
   contentType = 'text/csv'
 )
+
 ## download publication data
 output$downloadMsData <- downloadHandler(
   filename = function() {
@@ -471,6 +401,8 @@ output$downloadMsData <- downloadHandler(
   },
   contentType = "application/zip"
 )
+
+## STN: should this really be a fixed fileName ??
 output$downloadMsMsData <- downloadHandler(
   filename = function() {
     return("MSMS_library_showcase.msp")
@@ -481,6 +413,8 @@ output$downloadMsMsData <- downloadHandler(
   },
   contentType = "application/zip"
 )
+
+## STN: should this really be a fixed fileName ??
 output$downloadFragmentMatrix <- downloadHandler(
   filename = function() {
     return("Fragment_matrix_showcase.csv")
@@ -491,6 +425,7 @@ output$downloadFragmentMatrix <- downloadHandler(
   },
   contentType = "application/zip"
 )
+
 output$downloadDocShowcaseProtocol <- downloadHandler(
   filename = function() {
     return("MetFamily_Showcase_protocol.pdf")
@@ -501,6 +436,7 @@ output$downloadDocShowcaseProtocol <- downloadHandler(
   },
   contentType = "application/pdf"
 )
+
 output$downloadDocUserGuide <- downloadHandler(
   filename = function() {
     return("MetFamily_user_guide.pdf")
@@ -511,6 +447,7 @@ output$downloadDocUserGuide <- downloadHandler(
   },
   contentType = "application/pdf"
 )
+
 output$downloadDocInputSpecification <- downloadHandler(
   filename = function() {
     return("MetFamily_Input_Specification.pdf")
@@ -521,6 +458,7 @@ output$downloadDocInputSpecification <- downloadHandler(
   },
   contentType = "application/pdf"
 )
+
 #########################################################################################
 ## consensus spectrum for metabolite families
 output$downloadMetaboliteFamilyConsensusSpectrum <- downloadHandler(
@@ -529,7 +467,8 @@ output$downloadMetaboliteFamilyConsensusSpectrum <- downloadHandler(
   },
   content = function(file) {
     annotation <- allAnnotationNames[[input$familySelectionTable_rows_selected]]
-    precursorSet <- which(unlist(lapply(X = dataList$annoArrayOfLists, FUN = function(y){any(y==annotation)})))
+    precursorSet <- which(unlist(lapply(X = dataList$annoArrayOfLists, 
+                                        FUN = function(y){any(y==annotation)})))
     returnObj <- getSpectrumStatistics(dataList = dataList, precursorSet = precursorSet)
     fragmentMasses = returnObj$fragmentMasses
     fragmentCounts = returnObj$fragmentCounts
@@ -540,17 +479,20 @@ output$downloadMetaboliteFamilyConsensusSpectrum <- downloadHandler(
       "Frequency" = fragmentProportion
     )
     
-    write.table(x = consensusSpectrumDf, file = file, sep = "\t", row.names = FALSE, quote = FALSE)
+    write.table(x = consensusSpectrumDf, file = file, sep = "\t", 
+                row.names = FALSE, quote = FALSE)
   },
   contentType = 'text/csv'
 )
+
 output$downloadMetaboliteFamilyFilteredPrecursors <- downloadHandler(
   filename = function() {
     createMetaboliteFamilyProjectFileName(allAnnotationNames[[input$familySelectionTable_rows_selected]])
   },
   content = function(file) {
     annotation <- allAnnotationNames[[input$familySelectionTable_rows_selected]]
-    precursorSet <- which(unlist(lapply(X = dataList$annoArrayOfLists, FUN = function(y){any(y==annotation)})))
+    precursorSet <- which(unlist(lapply(X = dataList$annoArrayOfLists, 
+                                        FUN = function(y){any(y==annotation)})))
     writeTable(precursorSet = precursorSet, file = file)
   },
   contentType = 'text/csv'
@@ -579,9 +521,6 @@ createAnnotationResultTableAll <- function(){
     for(classIdx in seq_along(classToSpectra_class)){
       if(!(precursorIndex %in% as.integer(names(classToSpectra_class[[classIdx]]))))
         next
-      
-      #class          <- names(classToSpectra_class)[[selectedRowIdx]]
-      #classToSpectra <-       classToSpectra_class [[selectedRowIdx]]
       
       class <- names(classToSpectra_class)[[classIdx]]
       pValue <- format(unname( classToSpectra_class[[classIdx]][[which( precursorIndex == as.integer(names(classToSpectra_class[[classIdx]])) )]] ), digits=4)
@@ -612,6 +551,7 @@ createAnnotationResultTableAll <- function(){
   colnames(outputDf) <- head
   return(outputDf)
 }
+
 createAnnotationResultTableForClass <- function(){
   selectedRowIdx <- input$annotationResultTableClass_rows_selected
   
@@ -624,15 +564,16 @@ createAnnotationResultTableForClass <- function(){
   mzs                <- dataList$dataFrameInfos[precursorIndeces, "m/z"]
   rts                <- dataList$dataFrameInfos[precursorIndeces, "RT"]
   metaboliteNames    <- dataList$dataFrameInfos[precursorIndeces, "Metabolite name"]
-  presentAnnotations <- unlist(lapply(X = dataList$annoArrayOfLists[precursorIndeces], FUN = function(x){
-    if(length(x) == 0){
-      return("")
-    } else {
-      x <- sort(unlist(x))
-      s <- paste(x, collapse = "; ")
-      return(s)
-    }
-  }))
+  presentAnnotations <- unlist(lapply(X = dataList$annoArrayOfLists[precursorIndeces], 
+                                      FUN = function(x){
+                                        if(length(x) == 0){
+                                          return("")
+                                        } else {
+                                          x <- sort(unlist(x))
+                                          s <- paste(x, collapse = "; ")
+                                          return(s)
+                                        }
+                                      }))
   
   outputDf <- data.frame(
     "Index" = precursorIndeces, 
@@ -647,6 +588,7 @@ createAnnotationResultTableForClass <- function(){
   
   return(outputDf)
 }
+
 output$downloadAllAnnotationResults <- downloadHandler(
   filename = function() {
     createClassifierAnnotationName( "All" )
@@ -658,6 +600,7 @@ output$downloadAllAnnotationResults <- downloadHandler(
   },
   contentType = 'text/csv'
 )
+
 output$downloadMetaboliteFamilyAnnotationResults <- downloadHandler(
   filename = function() {
     createClassifierAnnotationName( names(classToSpectra_class)[[input$annotationResultTableClass_rows_selected]] )
