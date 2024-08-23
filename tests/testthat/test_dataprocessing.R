5b7f23bbd378c1e0dfedd51f943e7e23562bd1fb
Steffen Neumann <sneumann@ipb-halle.de>
2024-08-23 14:40:37 +0200

    Adding test for distance matrix

--- tests/testthat/test_dataprocessing.R
+++ tests/testthat/test_dataprocessing.R
@@ -23,3 +23,15 @@ test_that("metaboliteProfileParsing works", {
 expect_equal(min(result$dataFrameMeasurements[,1]), 0)
 
 })
+
+test_that("HCA works", {
+  load(system.file("extdata/testdata/calculateDistanceMatrix.Rdata", package = "MetFamily"))
+  
+  result <- calculateDistanceMatrix(dataList=dataList, 
+                                    filter=filter, 
+                                    distanceMeasure = "Jaccard", progress = FALSE)
+  expect_equal(sum(result$distanceMatrix), 46522.7, tolerance = 0.005)
+  expect_equal(sum(result$filter), 294508)
+  expect_true(result$distanceMeasure == "Jaccard")
+  
+})
