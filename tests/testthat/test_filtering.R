b08a719c8511de153f07bed3210e190dd05f5cf9
Norman Storz <norman.storz@student.uni-halle.de>
2025-02-26 09:27:09 +0100

    updated tests

--- tests/testthat/test_filtering.R
+++ tests/testthat/test_filtering.R
@@ -36,7 +36,7 @@ test_that("Filtering functions work with annotation data", {
   )
 
   # Test filtering using filterObj
-  filteredResult <- filterData(
+  filteredResult <- filterDataX(
     dataList = dataList,
     grouXXXps = filterObj$groupSet,
     sampleSet = filterObj$sampleSet,
@@ -89,7 +89,7 @@ test_that("Filtering functions work without annotation data", {
   )
 
   # Test filtering using filterObj
-  filteredResult <- filterData(
+  filteredResult <- filterDataX(
     dataList = dataList,
     grouXXXps = filterObj$groupSet,
     sampleSet = filterObj$sampleSet,
@@ -107,3 +107,103 @@ test_that("Filtering functions work without annotation data", {
   # Test that filtering worked as expected
   expect_equal(filteredResult$numberOfPrecursorsFiltered, 209)
 })
+
+
+
+#####debug custom function 
+
+filterDataX <- function(dataList, grouXXXps, sampleSet, filterBySamples, filter_average, filter_lfc, filterList_ms2_masses, filter_ms2_ppm, filter_ms1_masses, filter_ms1_ppm, includeIgnoredPrecursors, progress = FALSE){
+  
+  ##########################################
+  ## filter
+  filter <- rep(x = TRUE, times = dataList$numberOfPrecursors)
+   print(paste("Initial features:", sum(filter)))
+  
+  ## filter_average
+  if(!is.null(filter_average)){
+    filter <- filter & apply(
+      X = as.data.frame(dataList$dataFrameMeasurements[, sapply(as.vector(grouXXXps), FUN = dataList$dataMeanColumnNameFunctionFromName)]), 
+      MARGIN = 1, 
+      FUN = mean
+    ) >= filter_average
+     print(paste("After average filter:", sum(filter)))
+  }
+  
+  ## filter_lfc
+  if(!is.null(filter_lfc)){
+    if(filter_lfc != 0){
+      if(length(grouXXXps) != 2){  stop("The number of grouXXXps for LFC is not equal to two!") }
+      if(filter_lfc > 0)
+        filter <- filter & dataList$dataFrameMeasurements[, dataList$lfcColumnNameFunctionFromName(grouXXXps[[1]], grouXXXps[[2]])] >= filter_lfc
+      else
+        filter <- filter & dataList$dataFrameMeasurements[, dataList$lfcColumnNameFunctionFromName(grouXXXps[[1]], grouXXXps[[2]])] <= filter_lfc
+       print(paste("After LFC filter:", sum(filter)))
+    }
+  }
+  
+  ## filter_ms2_masses, filter_ms2_ppm
+  if(!is.null(filterList_ms2_masses) & !is.null(filter_ms2_ppm) & length(filterList_ms2_masses) > 0){
+    error <- abs(dataList$fragmentMasses) * filter_ms2_ppm / 1E6
+    filterFragmentLists <- rep(x = FALSE, times = dataList$numberOfPrecursors)
+    for(filter_ms2_masses in filterList_ms2_masses){
+      filterTempFragmentList <- rep(x = TRUE, times = dataList$numberOfPrecursors)
+      for(fragmentIndex in seq_len(length(filter_ms2_masses))){
+        distances <- abs(dataList$fragmentMasses - filter_ms2_masses[[fragmentIndex]])
+        filterTempFragment <- rep(x = FALSE, times = dataList$numberOfPrecursors)
+        columns <- which(distances <= error)
+        for(column in columns)
+          filterTempFragment <- filterTempFragment | dataList$featureMatrix[, column] != 0
+        filterTempFragmentList <- filterTempFragmentList & filterTempFragment
+      }
+      filterFragmentLists <- filterFragmentLists | filterTempFragmentList
+    }
+    filter <- filter & filterFragmentLists
+    print(paste("After MS2 filter:", sum(filter)))
+  }
+  
+  ## filter_ms1_masses, filter_ms1_ppm
+  if(!is.null(filter_ms1_masses) & !is.null(filter_ms1_ppm) & length(filter_ms1_masses) > 0){
+    precursorMasses <- as.numeric(dataList$dataFrameInfos[["m/z"]])
+    error <- precursorMasses * filter_ms1_ppm / 1E6
+    
+    filterMS1masses <- rep(x = FALSE, times = dataList$numberOfPrecursors)
+    for(precursorMassIndex in seq_len(length(filter_ms1_masses))){
+      distances <- abs(precursorMasses - filter_ms1_masses[[precursorMassIndex]])
+      filterPart <- distances <= error
+      filterPart[is.na(filterPart)] <- FALSE
+      filterMS1masses <- filterMS1masses | filterPart
+    }
+    filter <- filter & filterMS1masses
+     print(paste("After MS1 filter:", sum(filter)))
+  }
+  
+  ## include ignored precursors
+  if(!includeIgnoredPrecursors)
+    filter <- filter & !dataList$annoArrayIsArtifact
+   print(paste("After removing ignored precursors:", sum(filter)))
+  
+  filter <- which(filter)
+  
+  resultObj <- list()
+  resultObj$filter <- filter
+  resultObj$numberOfPrecursors <- dataList$numberOfPrecursors
+  resultObj$numberOfPrecursorsFiltered <- length(filter)
+  if(is.null(grouXXXps)){
+    resultObj$grouXXXps    <- list()
+    resultObj$sampleSet <- list()
+    resultObj$filterBySamples <- NA
+  } else {
+    resultObj$grouXXXps    <- grouXXXps
+    resultObj$sampleSet <- sampleSet
+    resultObj$filterBySamples <- filterBySamples
+  }
+  resultObj$filter_average           <- ifelse(is.null(filter_average), 0, filter_average)
+  resultObj$filter_lfc               <- ifelse(is.null(filter_lfc), 0, filter_lfc)
+  resultObj$filterList_ms2_masses    <- if(is.null(filterList_ms2_masses)) list() else filterList_ms2_masses
+  resultObj$filter_ms2_ppm           <- ifelse(is.null(filter_ms2_ppm), "", filter_ms2_ppm)
+  resultObj$filter_ms1_masses        <- if(is.null(filter_ms1_masses)) list() else filter_ms1_masses
+  resultObj$filter_ms1_ppm           <- ifelse(is.null(filter_ms1_ppm), "", filter_ms1_ppm)
+  resultObj$includeIgnoredPrecursors <- ifelse(is.null(includeIgnoredPrecursors), NA, includeIgnoredPrecursors)
+  
+  return (resultObj)
+}
--- tests/testthat/test_spectraImport.R
+++ tests/testthat/test_spectraImport.R
@@ -1,51 +1,135 @@
 library(testthat)
 
-dataList <<- list()
 
-# Set the file paths to test data 
-filePeakMatrixPath <- system.file("extdata/showcase/Metabolite_profile_showcase.txt", package = "MetFamily")
-fileSpectra        <- system.file("extdata/showcase/MSMS_library_showcase.msp", package = "MetFamily")
-fileAnnotation     <- system.file("extdata/testdata/canopus/canopusShort.txt", package = "MetFamily")
 
-# Load a saved parameter set.
-parameterSetPath <- system.file("extdata/testdata/parameterSet.RData", package = "MetFamily")
-load(parameterSetPath)  # Loads 'parameterSet'
-
-# Process data as in the app
-# Convert the raw files to a project file 
-resultObj <- convertToProjectFile(
-  filePeakMatrix = filePeakMatrixPath, 
-  fileSpectra      = fileSpectra,
-  fileAnnotation   = fileAnnotation,
-  parameterSet     = parameterSet, 
-  progress         = FALSE
-)
-
-# Convert the sparse matrix to a string
-lines <- sparseMatrixToString(
-  matrixRows = resultObj$matrixRows, 
-  matrixCols = resultObj$matrixCols, 
-  matrixVals = resultObj$matrixVals, 
-  parameterSet = parameterSet
-)
-qfeatures <- resultObj$qfeatures
-
-# Read the project data
-dataList <<- readProjectData(
-  fileLines = lines, 
-  progress  = FALSE, 
-  qfeatures = qfeatures
-)
-
-test_that("Data import produces the expected numbers", {
+test_that("Data import with annotations produces the expected numbers", {
+  dataList <<- list()
+  
+  # Set the file paths to test data 
+  filePeakMatrixPath <- system.file("extdata/showcase/Metabolite_profile_showcase.txt", package = "MetFamily")
+  fileSpectra        <- system.file("extdata/showcase/MSMS_library_showcase.msp", package = "MetFamily")
+  fileAnnotation     <- system.file("extdata/testdata/canopus/canopusShort.txt", package = "MetFamily")
+  
+  # Load the saved parameter set.
+  parameterSetPath <- system.file("extdata/testdata/parameterSet.RData", package = "MetFamily")
+  load(parameterSetPath)  # Loads 'parameterSet'
+  
+  # Process data as in the shiny app
+  resultObj <- convertToProjectFile(
+    filePeakMatrix = filePeakMatrixPath, 
+    fileSpectra      = fileSpectra,
+    fileAnnotation   = fileAnnotation,
+    parameterSet     = parameterSet, 
+    progress         = FALSE
+  )
+  
+  lines <- sparseMatrixToString(
+    matrixRows = resultObj$matrixRows, 
+    matrixCols = resultObj$matrixCols, 
+    matrixVals = resultObj$matrixVals, 
+    parameterSet = parameterSet
+  )
+  
+  qfeatures <- resultObj$qfeatures
+  
+  dataList <<- readProjectData(
+    fileLines = lines, 
+    progress  = FALSE, 
+    qfeatures = qfeatures
+  )
+  
+  # Test the message
+  #2460 / 5824 spectra were imported successfully 
   expect_equal(resultObj$numberOfParsedSpectra, 2640)        
-  expect_equal(resultObj$numberOfSpectraOriginal, 5824) #2460 / 5824 spectra were imported successfully        
+  expect_equal(resultObj$numberOfSpectraOriginal, 5824)       
+  
+  #(15 empty, 3163 low intensity, 5 too heavy)
   expect_equal(resultObj$numberOfSpectraDiscardedDueToNoPeaks, 15) 
-  #expect_equal(resultObj$numberOfSpectraDiscardedDueToMaxIntensity, 5)  
-  expect_equal(resultObj$numberOfSpectraDiscardedDueToTooHeavy, 5)     
-
-
-  #Check that the final dataList has been populated correctly.
-  expect_true(is.list(dataList))
-  expect_true("minimumMass" %in% names(dataList))
+  expect_equal(resultObj$numberOfSpectraDiscardedDueToMaxIntensity, 3163)
+  expect_equal(resultObj$numberOfSpectraDiscardedDueToTooHeavy, 5)
+  
+  #2414 / 2640 spectra were successfully mapped to MS¹ features
+  expect_equal(resultObj$numberOfPrecursors,2414)  
+  
+  #34369 / 145973 fragments were successfully imported.
+  expect_equal(resultObj$numberOfMS2PeaksAboveThreshold, 34369)
+  expect_equal(resultObj$numberOfMS2PeaksOriginal, 145973)
+  
+  #(1807 too heavy, 109797 low intensity)
+  expect_equal(resultObj$numberOfMS2PeaksBelowThreshold,109797 )
+  expect_equal(resultObj$numberOfTooHeavyFragments, 1807) 
+  
+  #2414 / 5823 MS¹ features were successfully imported
+  expect_equal(resultObj$numberOfParsedMs1Features, 5823) 
+  
+  #(420 were isotopes, 2989 without spectra)
+  expect_equal(resultObj$numberOfRemovedPrecursorIsotopePeaks, 420)
+  expect_equal(resultObj$numberOfUnmappedPrecursors, 2989) 
 })
+
+test_that("Data import without annotations produces the expected numbers", {
+  dataList <<- list()
+  
+  # Set the file paths to test data 
+  filePeakMatrixPath <- system.file("extdata/showcase/Metabolite_profile_showcase.txt", package = "MetFamily")
+  fileSpectra        <- system.file("extdata/showcase/MSMS_library_showcase.msp", package = "MetFamily")
+  
+  # Load the saved parameter set.
+  parameterSetPath <- system.file("extdata/testdata/parameterSet.RData", package = "MetFamily")
+  load(parameterSetPath)  # Loads 'parameterSet'
+  
+  # Process data as in the shiny app
+  resultObj <- convertToProjectFile(
+    filePeakMatrix = filePeakMatrixPath, 
+    fileSpectra      = fileSpectra,
+    fileAnnotation   =  NULL,  # No annotation
+    parameterSet     = parameterSet, 
+    progress         = FALSE
+  )
+  
+  lines <- sparseMatrixToString(
+    matrixRows = resultObj$matrixRows, 
+    matrixCols = resultObj$matrixCols, 
+    matrixVals = resultObj$matrixVals, 
+    parameterSet = parameterSet
+  )
+  
+  qfeatures <- resultObj$qfeatures
+  
+  dataList <<- readProjectData(
+    fileLines = lines, 
+    progress  = FALSE, 
+    qfeatures = qfeatures
+  )
+  
+  # Test the message
+  #2460 / 5824 spectra were imported successfully 
+  expect_equal(resultObj$numberOfParsedSpectra, 2640)        
+  expect_equal(resultObj$numberOfSpectraOriginal, 5824)       
+  
+  #(15 empty, 3163 low intensity, 5 too heavy)
+  expect_equal(resultObj$numberOfSpectraDiscardedDueToNoPeaks, 15) 
+  expect_equal(resultObj$numberOfSpectraDiscardedDueToMaxIntensity, 3163)
+  expect_equal(resultObj$numberOfSpectraDiscardedDueToTooHeavy, 5)
+  
+  #2414 / 2640 spectra were successfully mapped to MS¹ features
+  expect_equal(resultObj$numberOfPrecursors,2414)  
+  
+  #34369 / 145973 fragments were successfully imported.
+  expect_equal(resultObj$numberOfMS2PeaksAboveThreshold, 34369)
+  expect_equal(resultObj$numberOfMS2PeaksOriginal, 145973)
+  
+  #(1807 too heavy, 109797 low intensity)
+  expect_equal(resultObj$numberOfMS2PeaksBelowThreshold,109797 )
+  expect_equal(resultObj$numberOfTooHeavyFragments, 1807) 
+  
+  #2414 / 5823 MS¹ features were successfully imported
+  expect_equal(resultObj$numberOfParsedMs1Features, 5823) 
+  
+  #(420 were isotopes, 2989 without spectra)
+  expect_equal(resultObj$numberOfRemovedPrecursorIsotopePeaks, 420)
+  expect_equal(resultObj$numberOfUnmappedPrecursors, 2989) 
+
+})
+
+##TODO: Expot Import test 
