2f65c41a07be6ced0b1a9f4034e10d502fec34fa
Steffen Neumann <sneumann@ipb-halle.de>
2025-04-07 15:37:10 +0200

    Bump versions

--- inst/MetFamily/version.R
+++ inst/MetFamily/version.R
@@ -2,7 +2,7 @@
 
 toolName       <- "MetFamily"
 toolVersion    <- "1.0"    
-metFamilyAppVersion <- "1.3.7"
+metFamilyAppVersion <- "1.3.8"
 
 packageVersion <- Biobase::package.version("MetFamily")
 
