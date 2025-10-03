38221508156234563fdcfdde9bcc1cd693180223
Steffen Neumann <sneumann@ipb-halle.de>
2025-10-03 15:35:12 +0200

    Switch Logo to PNG

--- inst/MetFamily/ui.R
+++ inst/MetFamily/ui.R
@@ -7,7 +7,7 @@ crashButton = F
 
 
 navbarPage(
-  title = div(img(src='img/Metfamily.gif',style="margin-top: -14px; padding-right:10px;padding-bottom:10px", height = 60)),
+  title = div(img(src='img/MetFamily.png',style="margin-top: -14px; padding-right:10px;padding-bottom:10px", height = 60)),
   windowTitle="MetFamily",
   footer = HTML(readLines("www/ipbfooter.html")),
   
