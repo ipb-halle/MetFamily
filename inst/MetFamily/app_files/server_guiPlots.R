c282fe25bdf28e5c439a21d40705b3f689fd5c65
Guillaume Patoine <gpatoine@gmail.com>
2025-10-10 17:56:14 +0200

    fix minNbFrag issues with cosine-based similarity indices

--- inst/MetFamily/app_files/server_guiPlots.R
+++ inst/MetFamily/app_files/server_guiPlots.R
@@ -60,6 +60,8 @@ getSelectedPOI_XY <- function(mouseX, mouseY, poiCoordinatesX, poiCoordinatesY, 
 
 drawDendrogramPlotImpl <- function(){
   
+  browser()
+  
   resultObj <- calcPlotDendrogram(
     dataList = dataList, 
     filter = filterHca$filter, 
